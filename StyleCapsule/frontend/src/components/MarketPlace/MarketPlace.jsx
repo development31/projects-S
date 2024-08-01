febcc837500725b11217ec59241a64b7d79f3fd2
Shivangi Kumari <shivangi.kumari@aayanindia.in>
2024-08-01 16:45:11 +0530

    added MarketPlace

--- StyleCapsule/frontend/src/components/MarketPlace/MarketPlace.jsx
+++ StyleCapsule/frontend/src/components/MarketPlace/MarketPlace.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './MarketPlace.scss'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import one from './img/one.mp4'
 // import two from './img/two.png'
@@ -18,44 +18,43 @@ import one from './img/one.mp4'
 function MarketPlace() {
   return (
     <>
-    <div>
-    <div className='marketplace-navbar'>
-    <div className="navbar">
-      <div className="navbar-left">
-        <Link to='/login'><button className="login-btn">Log In</button></Link>
-        <Link to='/signup'><button className="signup-btn">Sign Up</button></Link>
-      </div>
-      <div className="navbar-center">
-        <h1 className="title">Style Capsule</h1>
-      </div>
-      <div className="navbar-right">
-      <i class="fa-regular fa-bell"></i>
-      <i class="fa-solid fa-magnifying-glass"></i>
-      <i class="fa-solid fa-cart-shopping"></i>
-      </div>
-    </div>
-    </div>
+      <div>
+        <div className='marketplace-navbar'>
+          <div className="navbar">
+            <div className="navbar-left">
+              <Link to='/login'><button className="login-btn">Log In</button></Link>
+              <Link to='/signup'><button className="signup-btn">Sign Up</button></Link>
+            </div>
+            <div className="navbar-center">
+              <h1 className="title">Style Capsule</h1>
+            </div>
+            <div className="navbar-right">
+              <i class="fa-regular fa-bell"></i>
+              <i class="fa-solid fa-magnifying-glass"></i>
+              <i class="fa-solid fa-cart-shopping"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className='marketplace1'>
+          <div className='vid'>
+            <video src={one} alt="Video" autoPlay loop muted />
+          </div>
+
+          {/* <div className='capsule'> */}
+          <div className='closet'>
+            <div className="marquee">
+              <p>REVOLUTIONIZE YOUR CLOSET</p>
+              <p>REVOLUTIONIZE YOUR CLOSET</p>
+              <p>REVOLUTIONIZE YOUR CLOSET</p>
+              <p>REVOLUTIONIZE YOUR CLOSET</p>
+            </div>
+          </div>
+          {/* </div> */}
 
 
-    <div className='marketplace1'>
-      <div className='vid'>
-        <video src={one} alt="Video" autoPlay loop muted />
+        </div>
       </div>
-
-      {/* <div className='capsule'> */}
-      <div className='closet'>
-    <div className="marquee">
-        <p>REVOLUTIONIZE YOUR CLOSET</p>
-        <p>REVOLUTIONIZE YOUR CLOSET</p>
-        <p>REVOLUTIONIZE YOUR CLOSET</p>
-        <p>REVOLUTIONIZE YOUR CLOSET</p>
-      </div>
-    </div>
-      {/* </div> */}
-
-      
-    </div>
-    </div>
     </>
   )
 }
